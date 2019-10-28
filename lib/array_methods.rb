b0451a2ed5e8b2df52480b9e2c08d2ed22fad789
Phil Newman <pnewman@viimed.com>
2019-10-28 16:19:33 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,9 +5,26 @@ end
 
 def find_max_value(array)
   max_value = 0 
-  array.length.times { |maxtest|
-  if maxtest > max_value
-    max_value = array[counter]
+  counter = 0
+  while array[counter] do
+    if array[counter] > max_value
+      max_value = array[counter]
+    end
+  counter += 1 
   end
+  max_value
+end
+
+
+def find_min_value(array)
+ min_value = 1000
+ counter = 0 
+ while array[counter] do
+   if array[counter] < min_value
+     min_value = array[counter]
+   end
+   counter -= 1 
+ end
+ min_value
 end
 
