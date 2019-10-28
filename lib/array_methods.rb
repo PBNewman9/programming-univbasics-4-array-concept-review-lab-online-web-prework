61ec95bae7311b0347e1f2033030f404ed76caa2
Phil Newman <pnewman@viimed.com>
2019-10-28 16:42:35 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,11 +2,11 @@ def find_element_index(array, value_to_find)
   counter = 0 
   while array[counter] do
     if array[counter]==value_to_find
-     array[counter].index 
+     array.index 
     end
     counter += 1
   end 
-  array[counter].index
+  value_to_find
 end
 
 
